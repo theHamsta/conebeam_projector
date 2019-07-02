6e2b24f3d55d7220bb8e40259e6a76b5825226c8
Stephan Seitz <stephan.seitz@fau.de>
2019-07-02 08:48:43 +0200

    Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    Dummy conftest.py for conebeam_projector.
+
+    If you don't know what this is for, just leave it empty.
+    Read more about conftest.py under:
+    https://pytest.org/latest/plugins.html
+"""
+
+# import pytest
